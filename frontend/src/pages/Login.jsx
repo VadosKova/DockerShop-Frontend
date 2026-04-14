a6ddfed800c3867b96b6d6641d98d4b9c4ad7970
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-04-14 16:14:51 +0300

    Created Login page

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,28 @@
+import { useState, useContext } from "react";
+import API from "../api/api";
+import { AuthContext } from "../context/AuthContext";
+
+export default function Login() {
+  const { setUser } = useContext(AuthContext);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = async () => {
+    const res = await API.post("/auth/login", { email, password });
+
+    localStorage.setItem("token", res.data.token);
+
+    const payload = JSON.parse(atob(res.data.token.split(".")[1]));
+    setUser(payload);
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input onChange={e => setEmail(e.target.value)} />
+      <input type="password" onChange={e => setPassword(e.target.value)} />
+      <button onClick={login}>Login</button>
+    </div>
+  );
+}
